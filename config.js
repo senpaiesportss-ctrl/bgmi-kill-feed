fa71df90c3f26ef1f7e2cf732e66f3b3d7c2481c
senpaiesportss-ctrl <senpaiesportss@gmail.com>
2026-09-04 03:01:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.KF_CONFIG={SUPABASE_URL:"https://nuywberanlfvngjnjpcx.supabase.co",SUPABASE_PUBLISHABLE_KEY:"sb_publishable_esq1befjL5m8GMlCsCB_ng_qY5t9GzB"};
+window.KF_CONFIG={SUPABASE_URL:"https://nuywberanlfvngjnjcpx.supabase.co",SUPABASE_PUBLISHABLE_KEY:"sb_publishable_esq1befjL5m8GMlCsCB_ng_qY5t9GzB"};
